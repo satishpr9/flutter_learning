7a2f780d86e68a47a82808807a7a488d833ed5df
Satish <you@example.com>
2020-11-04 19:11:48 +0530

    ch2 Complete

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,27 @@ class Homepage extends StatelessWidget {
       appBar: AppBar(
         title: Text("Hello"),
       ),
-      body: Container(child: Center(child: Text("Satish Pandey")),),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          alignment: Alignment.center,
+          height: 100,
+          width: 100,
+          decoration: BoxDecoration(
+            color: Colors.greenAccent,
+            borderRadius: BorderRadius.circular(10),
+            gradient: LinearGradient(
+              colors: [Colors.blue, Colors.red,] 
+              ),
+              boxShadow: [BoxShadow( 
+                blurRadius: 5,
+                color: Colors.grey[500],
+                offset: Offset(2.0,3.0)
+              )],
+          ),
+          child: Text("Satish",style: TextStyle(color:Colors.white,fontSize: 18 ),),
+        ),
+      ),
     );
   }
 }
